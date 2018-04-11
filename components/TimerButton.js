546b28c6cff7fc3ee0c39ebbe71b8f3df144a611
David Ryan <djrdev@icloud.com>
2018-04-10 20:26:53 -0600

    timer button styles

--- components/TimerButton.js
+++ components/TimerButton.js
@@ -19,3 +19,34 @@ export default function TimerButton({ color, title, small, onPress }) {
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 10,
+    minWidth: 100,
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+  small: {
+    fontSize: 14,
+    padding: 5,
+  },
+  large: {
+    fontSize: 16,
+    padding: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  elapsedTime: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 10,
+  }
+})
