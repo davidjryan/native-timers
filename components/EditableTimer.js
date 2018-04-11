f2554ac6b1176a4511771e7f26224b9a09fa6ed7
David Ryan <djrdev@icloud.com>
2018-04-10 19:16:26 -0600

    editabletimer component

---
+++ components/EditableTimer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import TimerFrom from './TimerForm';
+import Timer from './Timer';
+
+export default function EditableTimer({
+  id,
+  title,
+  project,
+  elapsed,
+  isRunning,
+  editFromOpen,
+}) {
+  if (editFormOpen) {
+    return (
+      <TimerForm 
+        id={id}
+        title={title}
+        project={project}
+        elapsed={elapsed}
+        isRunning={isRunning}
+      />
+    )  
+  };
+
+  
+}
