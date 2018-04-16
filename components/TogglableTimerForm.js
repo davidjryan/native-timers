46a045588a86e92670bbba3f4164f5175fa07683
David Ryan <djrdev@icloud.com>
2018-04-15 20:31:44 -0600

    togglabletimerform is now stateful

--- components/TogglableTimerForm.js
+++ components/TogglableTimerForm.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import TimerButton from './TimerButton';
 import TimerForm from './TimerForm';
 
-export default function TogglableTimerForm({ isOpen }) {
-  return (
-    <View style={[styles.container, !isOpen && styles.buttonPadding]}>
-      {isOpen ? <TimerForm /> : <TimerButton title="+" color="black" />}
-    </View>
-  )
+export default class TogglableTimerForm extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  handleFormOpen = () => {
+    this.setState({ isOpen: true });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    
+    return (
+      <View style={[styles.container, !isOpen && styles.buttonPadding]}>
+        {isOpen ? <TimerForm /> : <TimerButton title="+" color="black" onPress={this.handleFormOpen} />}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
