cd2f2b94ccbe8645519a334b2fe839a7a87cb832
David Ryan <djrdev@icloud.com>
2018-04-15 18:30:37 -0600

    app default state

--- App.js
+++ App.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import uuidv4 from 'uuid/v4';
 
 import EditableTimer from './components/EditableTimer';
 import TogglableTimerForm from './components/TogglableTimerForm';
 
 export default class App extends Component {
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: uuidv4(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuidv4(),
+        elapsed: 1273998,
+        isRunning: false,
+      }
+    ]
+  }
+
   render() {
+    const { timers } = this.state;
+    
     return (
       <View style={styles.appContainer}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Timers</Text>
         </View>
         <ScrollView style={styles.timerList}>
-          <TogglableTimerForm isOpen={false} />
+          <TogglableTimerForm />
+          {}
           <EditableTimer
             id="1"
             title="Mow the lawn"
             project="House Chores"
             elapsed="8986300"
             isRunning
-          />
-          <EditableTimer
-            id="2"
-            title="Bake Squash"
-            project="Kitchen Chores"
-            elapsed="3890985"
-            editFormOpen
           />
         </ScrollView>
       </View>
